823358695407aa324caa0a1323b24e9630c40dce
Michael Angelo Napoles <msnapoles@sscgi.com>
2026-08-07 11:22:50 +0800

    Initial commit

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.ComponentModel;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("First Name")]
+        public string FirstMidName { get; set; }
+        [DisplayName("Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        public string Secret { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
