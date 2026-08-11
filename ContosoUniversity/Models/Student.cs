abf250d5eed8080f51bc782f32500f3ded2bb50d
Michael Angelo Napoles <msnapoles@sscgi.com>
2026-08-11 15:10:05 +0800

    Implement Inheritance with EF in an ASP.NET MVC 5 app

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -7,35 +7,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Display(Name = "Last Name")]
-        [Required]
-        [StringLength(50, MinimumLength = 2)]
-        public string LastName { get; set; }
-
-        [Required]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        [Column("FirstName")]
-        [DisplayName("First Name")]
-        public string FirstMidName { get; set; }
-
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-        public string Secret { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
