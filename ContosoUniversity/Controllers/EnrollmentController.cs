5cf4c01f47551a8f21fbb18503d005a87173aaf6
Michael Angelo Napoles <msnapoles@sscgi.com>
2026-08-14 11:36:27 +0800

    Add feature initial prototype to Enroll to Course on Student module

---
+++ ContosoUniversity/Controllers/EnrollmentController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using ContosoUniversity.ViewModels;
+using Microsoft.Ajax.Utilities;
+
+namespace ContosoUniversity.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+        
+
+        // GET: Enrollment
+        //public ActionResult Index()
+        //{
+        //    var enrollments = db.Enrollments.Include(e => e.Course).Include(e => e.Student);
+        //    return View(enrollments.ToList());
+        //}
+
+        public ActionResult Index(int id)
+        {
+
+            ViewBag.studentID = id;
+
+            db.Database.Log = message => System.Diagnostics.Debug.WriteLine(message);
+
+            var enrolledcourse = db.Enrollments
+                .Where(e => e.StudentID == id)
+                .DistinctBy(e => e.CourseID)
+                .Select(e => e.CourseID)
+                .ToList();
+
+            var courses = db.Courses
+                .Include(e => e.Department)
+                .Where(c => !enrolledcourse.Contains(c.CourseID));
+
+            return View(courses.ToList());
+        }
+
+        // GET: Enrollment/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Create
+        public ActionResult Create(int? courseID, int? studentID)
+        {
+            var course = db.Courses.Find(courseID);
+            var student = db.Students.Find(studentID);
+
+            var coursetitle = course.Title;
+            var studentfullname = student.FullName;
+
+            ViewBag.CourseName = coursetitle; //new SelectList(db.Courses, "CourseID", "Title");
+            ViewBag.StudentName = studentfullname;// new SelectList(db.People, "ID", "FullName");
+
+            ViewBag.CourseID = courseID; //new SelectList(db.Courses, "CourseID", "Title");
+            ViewBag.StudentID = studentID;
+
+
+
+            return View();
+        }
+
+        // POST: Enrollment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EnrollmentID,CourseID,StudentID,Grade")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Student");
+            }
+
+            //ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+            //ViewBag.StudentID = new SelectList(db.People, "ID", "LastName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.People, "ID", "LastName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EnrollmentID,CourseID,StudentID,Grade")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", enrollment.CourseID);
+            ViewBag.StudentID = new SelectList(db.People, "ID", "LastName", enrollment.StudentID);
+            return View(enrollment);
+        }
+
+        // GET: Enrollment/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = db.Enrollments.Find(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = db.Enrollments.Find(id);
+            db.Enrollments.Remove(enrollment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        private void PopulateStudentCourseEnrollment(Enrollment enrollment)
+        {
+           //var 
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ContosoUniversity/ViewModels/StudentCourseEnrollment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class StudentCourseEnrollment
+    {
+        public int? CourseID { get; set; }
+        public int? StudentID { get; set; }
+
+
+    }
+}
